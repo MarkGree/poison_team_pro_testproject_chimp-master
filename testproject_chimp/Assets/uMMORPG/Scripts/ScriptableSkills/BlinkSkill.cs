76790b1cc0ce07f456de3a8c2ae604119843be41
MarkMa-LS <mazitovmark@gmail.com>
2024-03-05 15:32:24 +0500

    Fix2 - Blink

--- testproject_chimp/Assets/uMMORPG/Scripts/ScriptableSkills/BlinkSkill.cs
+++ testproject_chimp/Assets/uMMORPG/Scripts/ScriptableSkills/BlinkSkill.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "uMMORPG Skill/Blink", order = 999)]
 public class BlinkSkill : ScriptableSkill
 {
-    public override void Apply(Entity caster, int skillLevel, Vector2 direction)
+    public override void Apply(Entity caster, int skillLevel, Vector2 lookDirection)
     {
         Vector2 targetPosition;
 
         float range = base.castRange.Get(skillLevel);
-        if (NavMesh2D.Raycast(caster.transform.position, (Vector2)caster.transform.position + direction * range, out NavMeshHit2D hit, NavMesh2D.AllAreas))
+        Vector2 moveDirection = caster.movement.GetVelocity().normalized;
+
+        if (NavMesh2D.Raycast(caster.transform.position, (Vector2)caster.transform.position + moveDirection * range, out NavMeshHit2D hit, NavMesh2D.AllAreas))
         {
             targetPosition = hit.position;
         }
         else
-            targetPosition = (Vector2)caster.transform.position + direction * range;
+            targetPosition = (Vector2)caster.transform.position + moveDirection * range;
 
         caster.movement.Warp(targetPosition);
     }
 
     public override bool CheckDistance(Entity caster, int skillLevel, out Vector2 destination)
     {
-        destination = caster.transform.position + (Vector3)caster.lookDirection;
+        destination = (Vector2)caster.transform.position + caster.movement.GetVelocity().normalized;
         return true;
     }
 
