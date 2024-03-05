c34be9e8271224313a349824bc5848f4e4244f65
MarkMa-LS <mazitovmark@gmail.com>
2024-03-05 13:21:04 +0500

    Fix - Blink

--- testproject_chimp/Assets/uMMORPG/Scripts/ScriptableSkills/BlinkSkill.cs
+++ testproject_chimp/Assets/uMMORPG/Scripts/ScriptableSkills/BlinkSkill.cs
@@ -4,23 +4,14 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "uMMORPG Skill/Blink", order = 999)]
 public class BlinkSkill : ScriptableSkill
 {
-    static RaycastHit2D[] hitsBuffer = new RaycastHit2D[2];
-
     public override void Apply(Entity caster, int skillLevel, Vector2 direction)
     {
         Vector2 targetPosition;
 
-        // Фикс позиции, чтобы сервер не исправлял warp из-за попытки телепорта в стену
-        const float warpFix = 0.5f;
-
-        // Можно было ещё добавить, чтобы игнорировались мертвецы, но этого в условиях задачи не было
         float range = base.castRange.Get(skillLevel);
-        int hitsCount = Physics2D.RaycastNonAlloc(caster.transform.position, direction, hitsBuffer, range);
-        
-        if (hitsCount > 0)
+        if (NavMesh2D.Raycast(caster.transform.position, (Vector2)caster.transform.position + direction * range, out NavMeshHit2D hit, NavMesh2D.AllAreas))
         {
-            float distanceToHit = Vector2.Distance(caster.transform.position, hitsBuffer[1].point);
-            targetPosition = (Vector2)caster.transform.position + direction * (distanceToHit - warpFix);
+            targetPosition = hit.position;
         }
         else
             targetPosition = (Vector2)caster.transform.position + direction * range;
